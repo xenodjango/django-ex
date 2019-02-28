d662391cc01018f9d6ff731bebe50af6d9a87fdb
Spin <spinshanxi@gmail.com>
2019-02-28 14:02:49 +0800

    fix db setting error

--- project/database.py
+++ project/database.py
@@ -12,15 +12,15 @@ engines = {
 
 def config():
     service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
-    if service_name:
-        engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
-    else:
-        engine = engines['sqlite']
+    #if service_name:
+        #engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
+    #else:
+        #engine = engines['sqlite']
     name = os.getenv('DATABASE_NAME')
-    if not name and engine == engines['sqlite']:
-        name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
+    #if not name and engine == engines['sqlite']:
+        #name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
     engine = engines['postgresql']
-     
+
     return {
         'ENGINE': engine,
         'NAME': name,
